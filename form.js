63d326f5a85f1a7516033cd9eb6927bfa284dddc
unknown <fernandoclabbate@gmail.com>
2021-02-27 19:56:47 -0300

    UPDATE: padroninzaçao das aspas

--- form.js
+++ form.js
@@ -1,28 +1,28 @@
 // Input de piu para timeline
 
-const errorMessageSpan = document.querySelector('.error-message')
+const errorMessageSpan = document.querySelector(".error-message")
 
 const inputValidation = () => {
-    const form = document.querySelector('.form-piu');
+    const form = document.querySelector(".form-piu");
 
     const piu = form.userPiu.value;
 
     if (piu.length <= 0) {
-        return 'Não é possível enviar pius sem conteúdo!';
+        return "Não é possível enviar pius sem conteúdo!";
     }
     else if (piu.length > 140) {
-        return 'Não é possível enviar pius com mais de 140 caracteres!';
+        return "Não é possível enviar pius com mais de 140 caracteres!";
     }
     else {
         return false;
     }
 }
 
-const submitButton = document.querySelector('.submit-piu');
-submitButton.addEventListener('click', function(event) {
+const submitButton = document.querySelector(".submit-piu");
+submitButton.addEventListener("click", function(event) {
     event.preventDefault();
 
-    const form = document.querySelector('.form-piu');
+    const form = document.querySelector(".form-piu");
 
     const conteudoPiu = form.userPiu.value;
 
@@ -30,65 +30,65 @@ submitButton.addEventListener('click', function(event) {
 
     if (inputValidation()) {
         errorMessageSpan.textContent = errorMessage;
-        errorMessageSpan.classList.add('card');
+        errorMessageSpan.classList.add("card");
         return;
     }
 
-    errorMessageSpan.classList.remove('card');
-    errorMessageSpan.textContent = '';
+    errorMessageSpan.classList.remove("card");
+    errorMessageSpan.textContent = "";
 
-    const timeline = document.querySelector('.timeline-content');
+    const timeline = document.querySelector(".timeline-content");
             
-    const outsideDiv = document.createElement('div');
-    outsideDiv.classList.add('piu-content-outside');
+    const outsideDiv = document.createElement("div");
+    outsideDiv.classList.add("piu-content-outside");
 
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('piu-poster-info');
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("piu-poster-info");
 
-    const profilePicture = document.createElement('img');
-    const pictureLink = document.querySelector('.user-profile-picture')
+    const profilePicture = document.createElement("img");
+    const pictureLink = document.querySelector(".user-profile-picture")
     profilePicture.src = pictureLink.src;
-    profilePicture.alt = 'Foto de Perfil';
-    profilePicture.classList.add('profile-picture-feed');
+    profilePicture.alt = "Foto de Perfil";
+    profilePicture.classList.add("profile-picture-feed");
     infoDiv.appendChild(profilePicture);
 
-    const detailsDiv = document.createElement('div');
-    detailsDiv.classList.add('piu-details');
+    const detailsDiv = document.createElement("div");
+    detailsDiv.classList.add("piu-details");
 
-    const topDetailsDiv = document.createElement('div');
-    topDetailsDiv.classList.add('piu-details-top');
+    const topDetailsDiv = document.createElement("div");
+    topDetailsDiv.classList.add("piu-details-top");
 
-    const nameUserDiv = document.createElement('div');
-    nameUserDiv.classList.add('piu-name-username');
+    const nameUserDiv = document.createElement("div");
+    nameUserDiv.classList.add("piu-name-username");
 
-    const nomeP = document.createElement('p');
-    nomeP.classList.add('generic-name');
-    nomeP.classList.add('name-piu-feed');
+    const nomeP = document.createElement("p");
+    nomeP.classList.add("generic-name");
+    nomeP.classList.add("name-piu-feed");
     nomeP.textContent = "Fernando Labbate";
     nameUserDiv.appendChild(nomeP);
 
-    const userP = document.createElement('p');
-    userP.classList.add('generic-username');
-    userP.classList.add('username-piu-feed');
+    const userP = document.createElement("p");
+    userP.classList.add("generic-username");
+    userP.classList.add("username-piu-feed");
     userP.textContent = "@Felabbate";
     nameUserDiv.appendChild(userP);
     topDetailsDiv.appendChild(nameUserDiv);
 
-    /* const comP = document.createElement('p');                GAMBIARRA: tem que arrumar o espaçamento para ficar o mesmo em todos os pius
-    comP.classList.add('generic-username');
-    comP.textContent = '(comunidade)';
+    /* const comP = document.createElement("p");                GAMBIARRA: tem que arrumar o espaçamento para ficar o mesmo em todos os pius
+    comP.classList.add("generic-username");
+    comP.textContent = "(comunidade)";
     topDetailsDiv.appendChild(comP); */
 
-    const timeP = document.createElement('p');
-    timeP.classList.add('generic-username');
-    timeP.textContent = '1 s';
+    const timeP = document.createElement("p");
+    timeP.classList.add("generic-username");
+    timeP.textContent = "1 s";
     topDetailsDiv.appendChild(timeP);
 
-    const contentDiv = document.createElement('div');
-    contentDiv.classList.add('piu-content');
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("piu-content");
 
-    const piuP = document.createElement('p');
-    piuP.classList.add('piu-text-content');
+    const piuP = document.createElement("p");
+    piuP.classList.add("piu-text-content");
     piuP.textContent = conteudoPiu;
     contentDiv.appendChild(piuP);
 
@@ -98,59 +98,59 @@ submitButton.addEventListener('click', function(event) {
     infoDiv.appendChild(detailsDiv);
     outsideDiv.appendChild(infoDiv);
 
-    const reactionDiv = document.createElement('div');
-    reactionDiv.classList.add('piu-reaction-bar');
+    const reactionDiv = document.createElement("div");
+    reactionDiv.classList.add("piu-reaction-bar");
 
-    const reactionGroupDiv = document.createElement('div');
-    reactionGroupDiv.classList.add('reaction-group');
+    const reactionGroupDiv = document.createElement("div");
+    reactionGroupDiv.classList.add("reaction-group");
 
-    const likeButton = document.createElement('img');
-    likeButton.src = 'images/Favorite.svg';
-    likeButton.alt = 'Curtir';
-    likeButton.classList.add('btnFavorite');
+    const likeButton = document.createElement("img");
+    likeButton.src = "images/Favorite.svg";
+    likeButton.alt = "Curtir";
+    likeButton.classList.add("btnFavorite");
     reactionGroupDiv.appendChild(likeButton);
 
-    const likeNum = document.createElement('p');
-    likeNum.textContent = '0';
-    likeNum.classList.add('favorite-counter');
+    const likeNum = document.createElement("p");
+    likeNum.textContent = "0";
+    likeNum.classList.add("favorite-counter");
     reactionGroupDiv.appendChild(likeNum);
     reactionDiv.appendChild(reactionGroupDiv);
 
-    const reactionGroupDiv2 = document.createElement('div');
-    reactionGroupDiv2.classList.add('reaction-group');
+    const reactionGroupDiv2 = document.createElement("div");
+    reactionGroupDiv2.classList.add("reaction-group");
 
-    const commentButton = document.createElement('img');
-    commentButton.src = 'images/Chat_alt_add.svg';
-    commentButton.alt = 'Adicionar comentário';
+    const commentButton = document.createElement("img");
+    commentButton.src = "images/Chat_alt_add.svg";
+    commentButton.alt = "Adicionar comentário";
     reactionGroupDiv2.appendChild(commentButton);
 
-    const commentNum = document.createElement('p');
-    commentNum.textContent = '0';
+    const commentNum = document.createElement("p");
+    commentNum.textContent = "0";
     reactionGroupDiv2.appendChild(commentNum);
     reactionDiv.appendChild(reactionGroupDiv2);
 
-    const reactionGroupDiv3 = document.createElement('div');
-    reactionGroupDiv3.classList.add('reaction-group');
+    const reactionGroupDiv3 = document.createElement("div");
+    reactionGroupDiv3.classList.add("reaction-group");
 
-    const shareButton = document.createElement('img');
-    shareButton.src = 'images/Regroup.svg';
-    shareButton.alt = 'Repiuwpar';
+    const shareButton = document.createElement("img");
+    shareButton.src = "images/Regroup.svg";
+    shareButton.alt = "Repiuwpar";
     reactionGroupDiv3.appendChild(shareButton);
 
-    const shareNum = document.createElement('p');
-    shareNum.textContent = '0';
+    const shareNum = document.createElement("p");
+    shareNum.textContent = "0";
     reactionGroupDiv3.appendChild(shareNum);
     reactionDiv.appendChild(reactionGroupDiv3);
 
-    const salvarPiu = document.createElement('img');
-    salvarPiu.src = 'images/Bookmark.svg';
-    salvarPiu.alt = 'Salvar piu';
-    salvarPiu.classList.add('btnBookmark');
+    const salvarPiu = document.createElement("img");
+    salvarPiu.src = "images/Bookmark.svg";
+    salvarPiu.alt = "Salvar piu";
+    salvarPiu.classList.add("btnBookmark");
     reactionDiv.appendChild(salvarPiu);
 
-    const showCom = document.createElement('p');
-    showCom.classList.add('reaction-text');
-    showCom.textContent = 'Mostrar Conversa';
+    const showCom = document.createElement("p");
+    showCom.classList.add("reaction-text");
+    showCom.textContent = "Mostrar Conversa";
     reactionDiv.appendChild(showCom);
 
     outsideDiv.appendChild(infoDiv);
--- main.js
+++ main.js
@@ -8,57 +8,57 @@ const makeRequest = async(url) => {
         data.forEach(item => {
             arrayCounter++;
 
-            const timeline = document.querySelector('.timeline-content');
+            const timeline = document.querySelector(".timeline-content");
             
-            const outsideDiv = document.createElement('div');
-            outsideDiv.classList.add('piu-content-outside');
+            const outsideDiv = document.createElement("div");
+            outsideDiv.classList.add("piu-content-outside");
 
-            const infoDiv = document.createElement('div');
-            infoDiv.classList.add('piu-poster-info');
+            const infoDiv = document.createElement("div");
+            infoDiv.classList.add("piu-poster-info");
 
-            const profilePicture = document.createElement('img');
-            item.imagem ? (profilePicture.src = item.imagem) : (profilePicture.src = 'images/avatar.svg');
-            profilePicture.alt = 'Foto de Perfil';
-            profilePicture.classList.add('profile-picture-feed');
+            const profilePicture = document.createElement("img");
+            item.imagem ? (profilePicture.src = item.imagem) : (profilePicture.src = "images/avatar.svg");
+            profilePicture.alt = "Foto de Perfil";
+            profilePicture.classList.add("profile-picture-feed");
             infoDiv.appendChild(profilePicture);
 
-            const detailsDiv = document.createElement('div');
-            detailsDiv.classList.add('piu-details');
+            const detailsDiv = document.createElement("div");
+            detailsDiv.classList.add("piu-details");
 
-            const topDetailsDiv = document.createElement('div');
-            topDetailsDiv.classList.add('piu-details-top');
+            const topDetailsDiv = document.createElement("div");
+            topDetailsDiv.classList.add("piu-details-top");
 
-            const nameUserDiv = document.createElement('div');
-            nameUserDiv.classList.add('piu-name-username');
+            const nameUserDiv = document.createElement("div");
+            nameUserDiv.classList.add("piu-name-username");
 
-            const nomeP = document.createElement('p');
-            nomeP.classList.add('generic-name');
-            nomeP.classList.add('name-piu-feed');
+            const nomeP = document.createElement("p");
+            nomeP.classList.add("generic-name");
+            nomeP.classList.add("name-piu-feed");
             nomeP.textContent = item.nome;
             nameUserDiv.appendChild(nomeP);
 
-            const userP = document.createElement('p');
-            userP.classList.add('generic-username');
-            userP.classList.add('username-piu-feed');
+            const userP = document.createElement("p");
+            userP.classList.add("generic-username");
+            userP.classList.add("username-piu-feed");
             userP.textContent = item.username;
             nameUserDiv.appendChild(userP);
             topDetailsDiv.appendChild(nameUserDiv);
 
-            /* const comP = document.createElement('p');                GAMBIARRA: tem que arrumar o espaçamento para ficar o mesmo em todos os pius
-            comP.classList.add('generic-username');
-            comP.textContent = '(comunidade)';
+            /* const comP = document.createElement("p");                GAMBIARRA: tem que arrumar o espaçamento para ficar o mesmo em todos os pius
+            comP.classList.add("generic-username");
+            comP.textContent = "(comunidade)";
             topDetailsDiv.appendChild(comP); */
 
-            const timeP = document.createElement('p');
-            timeP.classList.add('generic-username');
+            const timeP = document.createElement("p");
+            timeP.classList.add("generic-username");
             timeP.textContent = `${arrayCounter} min`;
             topDetailsDiv.appendChild(timeP);
 
-            const contentDiv = document.createElement('div');
-            contentDiv.classList.add('piu-content');
+            const contentDiv = document.createElement("div");
+            contentDiv.classList.add("piu-content");
 
-            const piuP = document.createElement('p');
-            piuP.classList.add('piu-text-content');
+            const piuP = document.createElement("p");
+            piuP.classList.add("piu-text-content");
             piuP.textContent = item.mensagem;
             contentDiv.appendChild(piuP);
 
@@ -68,59 +68,59 @@ const makeRequest = async(url) => {
             infoDiv.appendChild(detailsDiv);
             outsideDiv.appendChild(infoDiv);
 
-            const reactionDiv = document.createElement('div');
-            reactionDiv.classList.add('piu-reaction-bar');
+            const reactionDiv = document.createElement("div");
+            reactionDiv.classList.add("piu-reaction-bar");
         
-            const reactionGroupDiv = document.createElement('div');
-            reactionGroupDiv.classList.add('reaction-group');
+            const reactionGroupDiv = document.createElement("div");
+            reactionGroupDiv.classList.add("reaction-group");
         
-            const likeButton = document.createElement('img');
-            likeButton.src = 'images/Favorite.svg';
-            likeButton.alt = 'Curtir';
-            likeButton.classList.add('btnFavorite');
+            const likeButton = document.createElement("img");
+            likeButton.src = "images/Favorite.svg";
+            likeButton.alt = "Curtir";
+            likeButton.classList.add("btnFavorite");
             reactionGroupDiv.appendChild(likeButton);
         
-            const likeNum = document.createElement('p');
+            const likeNum = document.createElement("p");
             likeNum.textContent = parseInt(10*Math.random());
-            likeNum.classList.add('favorite-counter');
+            likeNum.classList.add("favorite-counter");
             reactionGroupDiv.appendChild(likeNum);
             reactionDiv.appendChild(reactionGroupDiv);
         
-            const reactionGroupDiv2 = document.createElement('div');
-            reactionGroupDiv2.classList.add('reaction-group');
+            const reactionGroupDiv2 = document.createElement("div");
+            reactionGroupDiv2.classList.add("reaction-group");
         
-            const commentButton = document.createElement('img');
-            commentButton.src = 'images/Chat_alt_add.svg';
-            commentButton.alt = 'Adicionar comentário';
+            const commentButton = document.createElement("img");
+            commentButton.src = "images/Chat_alt_add.svg";
+            commentButton.alt = "Adicionar comentário";
             reactionGroupDiv2.appendChild(commentButton);
         
-            const commentNum = document.createElement('p');
+            const commentNum = document.createElement("p");
             commentNum.textContent = parseInt(10*Math.random());
             reactionGroupDiv2.appendChild(commentNum);
             reactionDiv.appendChild(reactionGroupDiv2);
         
-            const reactionGroupDiv3 = document.createElement('div');
-            reactionGroupDiv3.classList.add('reaction-group');
+            const reactionGroupDiv3 = document.createElement("div");
+            reactionGroupDiv3.classList.add("reaction-group");
         
-            const shareButton = document.createElement('img');
-            shareButton.src = 'images/Regroup.svg';
-            shareButton.alt = 'Repiuwpar';
+            const shareButton = document.createElement("img");
+            shareButton.src = "images/Regroup.svg";
+            shareButton.alt = "Repiuwpar";
             reactionGroupDiv3.appendChild(shareButton);
         
-            const shareNum = document.createElement('p');
+            const shareNum = document.createElement("p");
             shareNum.textContent = parseInt(10*Math.random());
             reactionGroupDiv3.appendChild(shareNum);
             reactionDiv.appendChild(reactionGroupDiv3);
         
-            const salvarPiu = document.createElement('img');
-            salvarPiu.src = 'images/Bookmark.svg';
-            salvarPiu.alt = 'Salvar piu';
-            salvarPiu.classList.add('btnBookmark');
+            const salvarPiu = document.createElement("img");
+            salvarPiu.src = "images/Bookmark.svg";
+            salvarPiu.alt = "Salvar piu";
+            salvarPiu.classList.add("btnBookmark");
             reactionDiv.appendChild(salvarPiu);
         
-            const showCom = document.createElement('p');
-            showCom.classList.add('reaction-text');
-            showCom.textContent = 'Mostrar Conversa';
+            const showCom = document.createElement("p");
+            showCom.classList.add("reaction-text");
+            showCom.textContent = "Mostrar Conversa";
             reactionDiv.appendChild(showCom);
 
             outsideDiv.appendChild(infoDiv);
@@ -135,4 +135,4 @@ const makeRequest = async(url) => {
     }
 }
 
-makeRequest('https://next.json-generator.com/api/json/get/EkyZfHLU_')
+makeRequest("https://next.json-generator.com/api/json/get/EkyZfHLU_")
